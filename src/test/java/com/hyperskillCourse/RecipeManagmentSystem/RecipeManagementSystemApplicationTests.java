2fbe8bdd0cb2979e39a4a37c335685f27984e6c0
abdulaziz545 <abdul45860@gmail.com>
2025-06-03 19:13:34 -0400

    Intitial project structure commit

---
+++ src/test/java/com/hyperskillCourse/RecipeManagmentSystem/RecipeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyperskillCourse.RecipeManagmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
