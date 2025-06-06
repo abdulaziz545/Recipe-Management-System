18d88accf40f9bcb25aea91b4952eaf420d40c96
abdulaziz545 <abdul45860@gmail.com>
2025-06-05 20:58:49 -0400

    Removed the jpa repository and made the application use a single recipe object

--- src/main/java/com/hyperskillCourse/RecipeManagmentSystem/businessLayer/Recipe.java
+++ src/main/java/com/hyperskillCourse/RecipeManagmentSystem/businessLayer/Recipe.java
@@ -1,39 +1,17 @@
 package com.hyperskillCourse.RecipeManagmentSystem.businessLayer;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-@Table(name = "recipe")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class Recipe {
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
-    @Column(name = "name")
     private String name;
-
-    @Column(name = "description")
     private String description;
-
-    @Column(name = "ingredients")
     private String ingredients;
-
-    @Column(name = "directions")
     private String directions;
-
-    public Recipe(String name, String description, String ingredients, String directions) {
-        this.name = name;
-        this.description = description;
-        this.ingredients = ingredients;
-        this.directions = directions;
-    }
 }
--- src/main/java/com/hyperskillCourse/RecipeManagmentSystem/serviceLayer/RecipeService.java
+++ src/main/java/com/hyperskillCourse/RecipeManagmentSystem/serviceLayer/RecipeService.java
@@ -1,34 +1,23 @@
 package com.hyperskillCourse.RecipeManagmentSystem.serviceLayer;
 
-import com.hyperskillCourse.RecipeManagmentSystem.businessLayer.MyMapper;
-import com.hyperskillCourse.RecipeManagmentSystem.businessLayer.RecipeDTO;
+
+import com.hyperskillCourse.RecipeManagmentSystem.businessLayer.Recipe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.hyperskillCourse.RecipeManagmentSystem.persistanceLayer.RecipeRepository;
 
-import java.util.NoSuchElementException;
+
 
 @Service
 public class RecipeService {
 
-    private final RecipeRepository recipeRepository;
-    private final MyMapper myMapper;
+    private Recipe recipe = new Recipe();
 
-    @Autowired
-    RecipeService(RecipeRepository recipeRepository, MyMapper myMapper) {
-        this.recipeRepository = recipeRepository;
-        this.myMapper = myMapper;
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
     }
 
-    public void saveRecipe(RecipeDTO recipeDTO) {
-        recipeRepository.deleteAll();
-        recipeRepository.save(myMapper.convertDTOToRecipe(recipeDTO));
-    }
-
-    public RecipeDTO getRecipe() {
-        return recipeRepository.findFirstBy()
-                .map(myMapper::convertRecipeDTO)
-                .orElseThrow(() -> new NoSuchElementException("No recipe found"));
+    public Recipe getRecipe() {
+        return recipe;
     }
 
 
